1d9792f5e23cebc0fc2d85702c6629351df34d54
Elena Nikitina <Kev_82@.ru>
2022-10-27 22:14:49 +0300

    Итоговое задание Dp3.6

--- NewsPortal/newsapp/views.py
+++ NewsPortal/newsapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['number_of_news'] = "Количество новостей:"
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news_id.html'
+    context_object_name = 'news_id'
+    pk_url_kwarg = 'id'
+
+
